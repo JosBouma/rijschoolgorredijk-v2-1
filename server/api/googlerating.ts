d18663fc42d03167d076c70ce57f86aa5504c00a
JosBouma <jos@loodzwaar-media.nl>
2023-11-16 23:30:36 +0100

    Rating poll

--- server/api/googlerating.ts
+++ server/api/googlerating.ts
@@ -1,18 +1,18 @@
-export default defineEventHandler(() => {
-    return {
-        result: {
-            rating: 5,
-            user_ratings_total: 2
-        }
-    }
-    // return $fetch(
-    //     'https://maps.googleapis.com/maps/api/place/details/json',
-    //     {
-    //         params: {
-    //             key: '???',
-    //             place_id: '????',
-    //             fields: 'rating,user_ratings_total'
-    //         }
+export default defineEventHandler(async () => {
+    // return {
+    //     result: {
+    //         rating: 5,
+    //         user_ratings_total: 2
     //     }
-    // );
+    // }
+    return await $fetch(
+        'https://maps.googleapis.com/maps/api/place/details/json',
+        {
+            params: {
+                key: process.env.PLACES_API_KEY,
+                place_id: 'ChIJXdF-n6P3eWQRcp4d6IUthyk',
+                fields: 'rating,user_ratings_total'
+            }
+        }
+    );
 })
